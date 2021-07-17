6902bda5a8de72b93e28ba7d0ca258574574cef0
이재원 <>
2021-07-17 17:02:37 +0900

    revome app.js

--- src/App.js
+++ src/App.js
@@ -8,16 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Signup />
-        <Login></Login>
-        <Logout></Logout>
-        <a 
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        />
+        
           
       </header>
     </div>
